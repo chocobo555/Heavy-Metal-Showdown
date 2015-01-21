f51e6dc26b8175c6f65316e358e18f5e39cd9a41
chocobo555 <ian.harris1@loop.colum.edu>
2015-01-20 20:04:27 -0600

    Jack's Movement

---
+++ RoboFighter/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour 
+{
+	public Transform target; //Player
+	public float smoothTime = 0.3F; //Camera Damp
+	private Vector3 velocity = Vector3.zero;
+
+
+	void Awake ()
+	{
+
+
+	}
+
+
+	void FixedUpdate ()
+	{
+		Vector3 targetPosition = target.TransformPoint(new Vector3(0, 0, -10));
+		transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+	}
+
+}
